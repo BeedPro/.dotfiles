46a3c5e12aa6d17b08b3710fd70f9b6adbcf2710
BeedPro <annoor28@hotmail.co.uk>
2025-12-21 21:13:17 +0000

    Add st src to share

---
+++ .local/share/st/patch/x_include.c
@@ -0,0 +1,8 @@
+/* Patches */
+#include "boxdraw.c"
+#include "drag-n-drop.c"
+#include "fixkeyboardinput.c"
+#include "fullscreen_x.c"
+#include "keyboardselect_reflow_x.c"
+#include "netwmicon.c"
+#include "st_embedder_x.c"
