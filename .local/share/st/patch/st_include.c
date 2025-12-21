d0e5eb5f35be30ae7d05ea257ec3b721810cd952
BeedPro <annoor28@hotmail.co.uk>
2025-12-21 21:13:17 +0000

    feat: st src to share

---
+++ .local/share/st/patch/st_include.c
@@ -0,0 +1,6 @@
+/* Patches */
+#include "externalpipe.c"
+#include "keyboardselect_reflow_st.c"
+#include "newterm.c"
+#include "reflow.c"
+#include "sync.c"
---
+++ .local/share/st/patch/x_include.h
@@ -0,0 +1,8 @@
+/* Patches */
+#include "boxdraw.h"
+#include "drag-n-drop.h"
+#include "fullscreen_x.h"
+#include "keyboardselect_reflow_st.h"
+#include "keyboardselect_reflow_x.h"
+#include "netwmicon.h"
+#include "st_embedder_x.h"
