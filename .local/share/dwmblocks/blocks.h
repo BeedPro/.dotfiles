8ff573b3a7e08f32550c9ba504f5a743da87e6e7
BeedPro <annoor28@hotmail.co.uk>
2025-12-29 18:37:04 +0000

    chore: update blocks.h to remove | from start

--- .local/share/dwmblocks/blocks.h
+++ .local/share/dwmblocks/blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"| ", "~/.local/scripts/xkblayout", 0, 20},
+	{" ", "~/.local/scripts/xkblayout", 0, 20},
 	{"/ ", "~/.local/scripts/disk", 30, 0},
 	{"", "~/.local/scripts/battery", 60, 0},
 	{"", "~/.local/scripts/datetime", 5, 0},
