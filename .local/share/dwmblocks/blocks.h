c779593e6c6cc25fdab5bf5691198d4db1ab23a3
BeedPro <annoor28@hotmail.co.uk>
2025-12-29 18:32:49 +0000

    Update dwmblocks to be more simple

--- .local/share/dwmblocks/blocks.h
+++ .local/share/dwmblocks/blocks.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "~/.local/scripts/xkblayout", 0, 20},
+	{"| ", "~/.local/scripts/xkblayout", 0, 20},
 	{"/ ", "~/.local/scripts/disk", 30, 0},
 	{"", "~/.local/scripts/battery", 60, 0},
 	{"", "~/.local/scripts/datetime", 5, 0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " :: ";
-static unsigned int delimLen = 5;
+static char delim[] = " | ";
+static unsigned int delimLen = 3;
