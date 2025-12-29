0faf37597dcc9b0a1ba8c5b44f3fe8f63fdfcf30
BeedPro <annoor28@hotmail.co.uk>
2025-12-29 17:18:48 +0000

    Remove widgets from bar

--- .local/share/dwmblocks/blocks.h
+++ .local/share/dwmblocks/blocks.h
@@ -2,10 +2,8 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{" ", "~/.local/scripts/xkblayout", 0, 20},
-	{"root ", "~/.local/scripts/disk", 30, 0},
+	{"/ ", "~/.local/scripts/disk", 30, 0},
 	{"", "~/.local/scripts/battery", 60, 0},
-	{"", "~/.local/scripts/brightness", 0, 15},
-	{"", "~/.local/scripts/volume", 0, 10},
 	{"", "~/.local/scripts/datetime", 5, 0},
 };
 
