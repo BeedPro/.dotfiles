477b6671619c833b529adf7247ce4ae732171d48
BeedPro <annoor28@hotmail.co.uk>
2026-01-01 21:34:32 +0000

    feat: pluigns/init

--- .config/bim/lua/plugins/init.lua
+++ .config/bim/lua/plugins/init.lua
@@ -3,4 +3,39 @@ return {
     "alexghergh/nvim-tmux-navigation",
     lazy = false,
   },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    build = ":TSUpdate",
+    opts = function()
+      require "configs.treesitter" 
+    end,
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    opts = require "configs.conform",
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPost", "BufWritePost", "InsertLeave", "BufEnter" },
+    config = function()
+      require "configs.nvim-lint"
+    end,
+  },
 }
