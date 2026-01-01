09e4d3c44fb8811864813a6324754a3e2fea0817
BeedPro <annoor28@hotmail.co.uk>
2026-01-01 20:38:28 +0000

    chore: Seperate out config and plugin for catppuccin

--- .config/bim/lua/plugins/catppuccin.lua
+++ .config/bim/lua/plugins/catppuccin.lua
@@ -4,23 +4,6 @@ return {
   lazy = false,
   priority = 1000, 
   config = function()
-    require("catppuccin").setup({
-      flavour = "auto", 
-      background = {
-        light = "latte",
-        dark = "mocha",
-      },
-      no_italic = true,
-      transparent_background = false,
-      term_colors = true,
-      integrations = {
-        treesitter = true,
-        gitsigns = true,
-        telescope = true,
-        nvimtree = true,
-      },
-    })
-
-    vim.cmd.colorscheme("catppuccin")
+    require("configs.catppuccin")
   end,
 }
