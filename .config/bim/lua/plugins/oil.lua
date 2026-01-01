d2b9918fc0cdc44cf768cea482418a34bb6d23c2
BeedPro <annoor28@hotmail.co.uk>
2026-01-01 20:29:31 +0000

    chore: Init bim

---
+++ .config/bim/lua/plugins/oil.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    config = function()
+      local oil = require "oil"
+      oil.setup {
+        delete_to_trash = true,
+        view_options = {
+          show_hidden = true,
+        },
+      }
+    end,
+    cmd = "Oil",
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  },
+}
+
