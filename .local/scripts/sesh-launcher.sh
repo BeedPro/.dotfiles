c913df70cb976e1951d3d3407e4b966f15f77c67
BeedPro <annoor28@hotmail.co.uk>
2026-01-03 13:07:33 +0000

    chore: remove icons from sesh

--- .local/scripts/sesh-launcher.sh
+++ .local/scripts/sesh-launcher.sh
@@ -17,17 +17,17 @@ ensure_session () {
 }
 
 # Prompt the user with a fuzzy finder interface to select a session
-selected=$(sesh list --icons | fzf-tmux -p 80%,70% \
+selected=$(sesh list | fzf-tmux -p 80%,70% \
   --ansi \
   --no-sort \
   --border-label ' sesh ' --prompt 'all ' \
   --header '  (C-a) all :: (C-t) tmux :: (C-g) config :: (C-x) zoxide :: (M-bs) kill' \
   --bind 'tab:down,btab:up' \
-  --bind 'ctrl-a:change-prompt(all )+reload(sesh list --icons)' \
-  --bind 'ctrl-t:change-prompt(ses )+reload(sesh list -t --icons)' \
-  --bind 'ctrl-g:change-prompt(dir )+reload(sesh list -c --icons)' \
-  --bind 'ctrl-x:change-prompt(zox )+reload(sesh list -z --icons)' \
-  --bind 'alt-backspace:execute(tmux kill-session -t {2..})+change-prompt(all )+reload(sesh list --icons)' \
+  --bind 'ctrl-a:change-prompt(all )+reload(sesh list)' \
+  --bind 'ctrl-t:change-prompt(ses )+reload(sesh list -t)' \
+  --bind 'ctrl-g:change-prompt(dir )+reload(sesh list -c)' \
+  --bind 'ctrl-x:change-prompt(zox )+reload(sesh list -z)' \
+  --bind 'alt-backspace:execute(tmux kill-session -t {2..})+change-prompt(all )+reload(sesh list)' \
   --border=sharp \
   --preview-border=left \
   --no-scrollbar \
