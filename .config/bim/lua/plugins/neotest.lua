64361b309a0523ac746b45f748f571a3c48a6d5b
BeedPro <annoor28@hotmail.co.uk>
2026-01-04 03:52:39 +0000

    Add neotest

---
+++ .config/bim/lua/plugins/neotest.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require "configs.neotest"
+    end,
+  },
+  {
+    "nvim-neotest/neotest-python",
+    dependencies = "nvim-neotest/neotest",
+  },
+
+  {
+    "nvim-neotest/neotest-plenary",
+    dependencies = "nvim-neotest/neotest",
+  },
+  {
+    "Issafalcon/neotest-dotnet",
+    dependencies = "nvim-neotest/neotest",
+  },
+
+  {
+    "rcasia/neotest-java",
+    ft = "java",
+    dependencies = {
+      "mfussenegger/nvim-jdtls",
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+    },
+  },
+}
