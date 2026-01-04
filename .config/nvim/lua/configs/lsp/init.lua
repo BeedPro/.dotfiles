f20ce62c015a11ca43d1c4ab207e8cc041abefea
BeedPro <annoor28@hotmail.co.uk>
2026-01-04 21:15:28 +0000

    Setup statusline for nvim

--- .config/nvim/lua/configs/lsp/init.lua
+++ .config/nvim/lua/configs/lsp/init.lua
@@ -19,7 +19,7 @@ function M.setup()
     on_init = client.on_init,
   })
 
-  local servers = { "lua_ls", "pyright", "clangd", "tinymist", "roslyn", "tailwindcss" }
+  local servers = { "lua_ls", "pyright", "clangd", "tinymist", "roslyn", "tailwindcss", "svelte" }
 
   for _, server in ipairs(servers) do
     local ok, config = pcall(require, "configs.lsp.servers." .. server)
