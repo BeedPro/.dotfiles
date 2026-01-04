807add9f45886731cfea9d67664ed257a9fd3d23
BeedPro <annoor28@hotmail.co.uk>
2026-01-04 19:40:13 +0000

    Remove redudant options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -47,7 +47,3 @@ g.loaded_node_provider = 0
 g.loaded_python3_provider = 0
 g.loaded_perl_provider = 0
 g.loaded_ruby_provider = 0
-
-local is_windows = vim.fn.has "win32" ~= 0
-local sep = is_windows and "\\" or "/"
-local delim = is_windows and ";" or ":"
