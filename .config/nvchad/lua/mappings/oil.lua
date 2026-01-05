f2ab665c607562b0b5357f8bb032245504c83bf6
BeedPro <annoor28@hotmail.co.uk>
2026-01-05 11:35:46 +0000

    chore: remove nvchad from dotfiles

--- .config/nvchad/lua/mappings/oil.lua
+++
@@ -1,9 +0,0 @@
-local map = vim.keymap.set
-
-map("n", "<leader>.", function()
-  if vim.bo.filetype == "oil" then
-    require("oil").close()
-  else
-    require("oil").open()
-  end
-end, { desc = "Toggle Oil file explorer" })
