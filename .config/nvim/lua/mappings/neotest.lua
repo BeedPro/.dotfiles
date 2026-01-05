ac42cc53711a4bbb00fa3308e415db7ad1f44d85
BeedPro <annoor28@hotmail.co.uk>
2026-01-05 01:12:34 +0000

    Fix bug in mappings/neotest.lua

--- .config/nvim/lua/mappings/neotest.lua
+++ .config/nvim/lua/mappings/neotest.lua
@@ -1,20 +1,21 @@
 local map = vim.keymap.set
-local neotest = require "neotest"
 
-map("n", "<leader>tr", neotest.run.run, { desc = "[T]est [R]un" })
+map("n", "<leader>tr", function()
+  require("neotest").run.run()
+end, { desc = "[T]est [R]un" })
 
 map("n", "<leader>tc", function()
-  neotest.run.run(vim.fn.expand "%")
+  require("neotest").run.run(vim.fn.expand "%")
 end, { desc = "[T]est [C]urrent file" })
 
 map("n", "<leader>td", function()
-  neotest.run.run { suite = false, strategy = "dap" }
+  require("neotest").run.run { suite = false, strategy = "dap" }
 end, { desc = "[T]est [D]ebug nearest" })
 
 map("n", "<leader>ts", function()
-  neotest.run.stop()
+  require("neotest").run.stop()
 end, { desc = "[T]est [S]top" })
 
 map("n", "<leader>ta", function()
-  neotest.run.attach()
+  require("neotest").run.attach()
 end, { desc = "[T]est [A]ttach" })
