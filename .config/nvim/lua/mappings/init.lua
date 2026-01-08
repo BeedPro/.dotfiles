5030bcfdd3bab5b0afba55b7e200bf50d624aa69
BeedPro <annoor28@hotmail.co.uk>
2026-01-08 14:44:11 +0000

    chore: remove useless keybinds neovim

--- .config/nvim/lua/mappings/init.lua
+++ .config/nvim/lua/mappings/init.lua
@@ -12,23 +12,13 @@ require "mappings.conform"
 
 local map = vim.keymap.set
 
-map("n", "<C-Up>", ":resize +2<CR>", { desc = "[W]indow [H]eight +" })
-map("n", "<C-Down>", ":resize -2<CR>", { desc = "[W]indow [H]eight -" })
-map("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "[W]indow [W]idth -" })
-map("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "[W]indow [W]idth +" })
-
 map({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr>", { desc = "[S]ave [F]ile" })
+map("n", "<C-a>", "ggVG", { desc = "[S]elect [A]ll" })
+map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "[C]opy [A]ll" })
+map("n", "<Esc>", "<cmd>noh<CR>", { desc = "[C]lear [H]ighlights" })
 
 map({ "n", "v" }, "<leader>y", '"+y', { desc = "[Y]ank to clipboard" })
 map({ "n", "v" }, "P", '"+p', { desc = "[P]aste from clipboard" })
-
-map("n", "<Esc>", "<cmd>noh<CR>", { desc = "[C]lear [H]ighlights" })
-map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "[C]opy [A]ll" })
-
-map("n", "<leader>/", "gcc", { desc = "[C]omment [T]oggle", remap = true })
-map("v", "<leader>/", "gc", { desc = "[C]omment [T]oggle", remap = true })
-
-map("n", "<leader>cd", "<cmd>cd %:p:h<CR>", { desc = "[C]hange [D]irectory" })
 
 map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "[D]iagnostic [S]how (loclist)" })
 map("n", "<leader>da", vim.diagnostic.setqflist, { desc = "[D]iagnostic [A]all (globallist)" })
