d94a110a9750df742fe9f173dec972d52a9c1a09
Beed <annoor28@hotmail.co.uk>
2026-01-11 23:27:38 +0000

    Refactor config of dap-python to configs.dap.adapters.python

--- .config/nvim/lua/mappings/dap.lua
+++ .config/nvim/lua/mappings/dap.lua
@@ -10,11 +10,14 @@ map("n", "<leader>d?", function()
     height = 10,
   })
 end, { desc = "[D]ap Question[?]" })
-map("n", "<F5>", dap.continue, { desc = "Dap Continue" })
-map("n", "<F10>", dap.step_over, { desc = "Dap Step Over" })
-map("n", "<F11>", dap.step_into, { desc = "Dap Step Into" })
-map("n", "<F12>", dap.step_out, { desc = "Dap Step Out" })
+
+map("n", "<leader>dc", dap.continue, { desc = "[D]ap [C]ontinue" })
+map("n", "<leader>dn", dap.step_over, { desc = "[D]ap [N]ext (Step Over)" })
+map("n", "<leader>di", dap.step_into, { desc = "[D]ap [I]nto (Step Into)" })
+map("n", "<leader>do", dap.step_out, { desc = "[D]ap [O]ut (Step Out)" })
+
 map("n", "<Leader>db", dap.toggle_breakpoint, { desc = "[D]ap Toggle [B]reakpoint" })
 map("n", "<Leader>dB", dap.set_breakpoint, { desc = "[D]ap Set [B]reakpoint" })
 map("n", "<Leader>dr", dap.repl.open, { desc = "[D]ap [R]epl Open" })
 map("n", "<Leader>dl", dap.run_last, { desc = "[D]ap Run [L]ast" })
+map("n", "<Leader>dt", dap.terminate, { desc = "[D]ap [T]erminate" })
