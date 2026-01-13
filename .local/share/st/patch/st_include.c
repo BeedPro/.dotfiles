68f941ef7ab892887d0b76d8e152a86b1850e64c
Beed <annoor28@hotmail.co.uk>
2026-01-13 01:08:13 +0000

    Remove suckless software

--- .local/share/st/patch/st_include.c
+++
@@ -1,6 +0,0 @@
-/* Patches */
-#include "externalpipe.c"
-#include "keyboardselect_reflow_st.c"
-#include "newterm.c"
-#include "reflow.c"
-#include "sync.c"
--- .local/share/st/patch/st_include.h
+++
@@ -1,6 +0,0 @@
-/* Patches */
-#include "externalpipe.h"
-#include "keyboardselect_reflow_st.h"
-#include "newterm.h"
-#include "reflow.h"
-#include "sync.h"
