a7ff5c2866f13493380439125ab1da679d3f40aa
Beed <annoor28@hotmail.co.uk>
2026-01-15 19:55:52 +0000

    Add nvim-dap-virtual-text setup to dap/ui

--- .config/nvim/lua/configs/dap/ui.lua
+++ .config/nvim/lua/configs/dap/ui.lua
@@ -5,3 +5,5 @@ sign("DapBreakpointCondition", { text = "B", texthl = "DapBreakpointCondition" }
 sign("DapLogPoint", { text = "L", texthl = "DapLogPoint" })
 sign("DapStopped", { text = ">", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
 sign("DapBreakpointRejected", { text = "X", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
+
+require("nvim-dap-virtual-text").setup {}
