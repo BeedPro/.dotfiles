ac81bd989ab5f241b82e07e1c965b01755868538
Beed <annoor28@hotmail.co.uk>
2026-01-15 17:48:56 +0000

    feat: class docstring for python

--- .config/nvim/snippets/luasnips/python.lua
+++ .config/nvim/snippets/luasnips/python.lua
@@ -1,5 +1,73 @@
 local ts_utils = require "nvim-treesitter.ts_utils"
 
+local function get_init_function(class_node)
+  for child in class_node:iter_children() do
+    if child:type() == "block" then
+      for stmt in child:iter_children() do
+        if stmt:type() == "function_definition" then
+          for name in stmt:iter_children() do
+            if name:type() == "identifier" then
+              if vim.treesitter.get_node_text(name, 0) == "__init__" then
+                return stmt
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+local function get_class_node_at_cursor()
+  local node = ts_utils.get_node_at_cursor()
+  while node do
+    if node:type() == "class_definition" then
+      return node
+    end
+    node = node:parent()
+  end
+end
+
+local function get_class_init_params()
+  local class_node = get_class_node_at_cursor()
+  if not class_node then
+    return nil
+  end
+
+  local init_fn = get_init_function(class_node)
+  if not init_fn then
+    return {}
+  end
+
+  -- reuse existing logic
+  local params = {}
+
+  for child in init_fn:iter_children() do
+    if child:type() == "parameters" then
+      for param in child:iter_children() do
+        local t = param:type()
+
+        if t == "identifier" then
+          table.insert(params, vim.treesitter.get_node_text(param, 0))
+        elseif t == "default_parameter" or t == "typed_parameter" or t == "typed_default_parameter" then
+          for c in param:iter_children() do
+            if c:type() == "identifier" then
+              table.insert(params, vim.treesitter.get_node_text(c, 0))
+              break
+            end
+          end
+        end
+      end
+    end
+  end
+
+  if params[1] == "self" then
+    table.remove(params, 1)
+  end
+
+  return params
+end
+
 local function is_class_method(fn_node)
   if not fn_node then
     return false
@@ -89,7 +157,7 @@ local sn = ls.snippet_node
 local t = ls.text_node
 
 local function python_docstring()
-  local params = get_python_params()
+  local params = get_class_init_params() or get_python_params()
 
   local nodes = {
     t '"""',
