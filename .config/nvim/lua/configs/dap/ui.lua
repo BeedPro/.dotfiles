401c64bfb3768feb217783f3d79965d7cdbe1783
BeedPro <annoor28@hotmail.co.uk>
2026-01-15 16:41:22 +0000

    Add highlight to dapstopped

--- .config/nvim/lua/configs/dap/ui.lua
+++ .config/nvim/lua/configs/dap/ui.lua
@@ -3,5 +3,5 @@ local sign = vim.fn.sign_define
 sign("DapBreakpoint", { text = "B", texthl = "DapBreakpoint" })
 sign("DapBreakpointCondition", { text = "B", texthl = "DapBreakpointCondition" })
 sign("DapLogPoint", { text = "L", texthl = "DapLogPoint" })
-sign("DapStopped", { text = ">", texthl = "DapStopped" })
+sign("DapStopped", { text = ">", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
 sign("DapBreakpointRejected", { text = "X", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
