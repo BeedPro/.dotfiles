9c8a3c26c8c60952d5f8424db8386f0290792b55
Beed <annoor28@hotmail.co.uk>
2026-01-15 03:24:11 +0000

    chore: remove git mappings neovim

--- .config/nvim/lua/mappings/snacks.lua
+++ .config/nvim/lua/mappings/snacks.lua
@@ -1,14 +1,6 @@
 local map = vim.keymap.set
 local snacks = require "snacks"
 
-map("n", "<leader>gl", function()
-  snacks.lazygit.log()
-end, { desc = "Lazy[G]it [L]og" })
-
-map("n", "<leader>gf", function()
-  snacks.lazygit.log_file()
-end, { desc = "Lazy[g]it Log [F]ile" })
-
 map("n", "<leader>n", function()
   snacks.notifier.show_history()
 end, { desc = "[N]otification History" })
