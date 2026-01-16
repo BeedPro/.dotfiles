f2dbc918581cdd5c44c6d49cb64dd6624154885e
Beed <annoor28@hotmail.co.uk>
2026-01-16 23:15:53 +0000

    chore: Renmove borders to signature and use telescope for lsp

--- .config/nvim/lua/configs/lsp/attach.lua
+++ .config/nvim/lua/configs/lsp/attach.lua
@@ -1,5 +1,7 @@
 local M = {}
 local map = vim.keymap.set
+local lspbuf = vim.lsp.buf
+local telescope = require "telescope.builtin"
 
 -- export on_attach & capabilities
 M.on_attach = function(_, bufnr)
@@ -7,21 +9,14 @@ M.on_attach = function(_, bufnr)
     return { buffer = bufnr, desc = desc }
   end
 
-  map("n", "gD", vim.lsp.buf.declaration, opts "[G]oto [D]eclaration")
-  map("n", "gd", vim.lsp.buf.definition, opts "[G]oto [d]efinition")
-  map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts "[W]orkspace [A]dd")
-  map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts "[W]orkspace [R]emove")
-  map("n", "grn", vim.lsp.buf.rename, opts "[R]e[n]ame")
-  map("n", "K", function()
-    vim.lsp.buf.hover { border = "single", max_height = 25, max_width = 120 }
-  end, opts "[H]over [D]ocs")
-
-  map("n", "<leader>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, opts "[W]orkspace [L]ist")
-
-  map("n", "<leader>ra", require "mappings.renamer", opts "[R]ename [A]ll")
-  map("n", "<leader>ca", vim.lsp.buf.code_action, opts "[C]ode [A]ction")
+  map("n", "grn", require "mappings.renamer", opts "[R]e[n]ame")
+  map("n", "gra", lspbuf.code_action, opts "[C]ode [A]ction")
+  map("n", "grr", telescope.lsp_references, opts "[C]ode [R]eferences")
+  map("n", "gri", telescope.lsp_implementations, opts "[C]ode [I]mplementation")
+  map("n", "grt", telescope.lsp_type_definitions, opts "[C]ode [T]ype Definition")
+  map("n", "gO", telescope.lsp_document_symbols, opts "[C]ode Symb[O]ls")
+  map("n", "gD", lspbuf.declaration, opts "[G]oto [D]eclaration")
+  map("n", "gd", telescope.lsp_definitions, opts "[G]oto [d]efinition")
 end
 
 return M
