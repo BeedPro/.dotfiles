48fc62fc072f300d36073ac0a510e82258bbfc6b
Beed <annoor28@hotmail.co.uk>
2026-01-17 00:07:07 +0000

    chore: Upate how options look

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -13,8 +13,9 @@ o.cursorline = true
 o.cursorlineopt = "both"
 
 o.expandtab = true
-o.shiftwidth = 2
+o.autoindent = true
 o.smartindent = true
+o.shiftwidth = 2
 o.tabstop = 2
 o.softtabstop = 2
 
