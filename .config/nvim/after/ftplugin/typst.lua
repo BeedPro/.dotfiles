5aa3f5d5f35d0577e79c0daaba84fca96369f304
Beed <annoor28@hotmail.co.uk>
2026-01-18 00:19:10 +0000

    Add typst mapping to after

--- .config/nvim/after/ftplugin/typst.lua
+++ .config/nvim/after/ftplugin/typst.lua
@@ -1,3 +1,14 @@
 local o = vim.o
 o.spell = true
 o.spelllang = "en_gb"
+
+local map = vim.keymap.set
+local builtin = require "telescope.builtin"
+
+map("n", "<space>frb", function()
+  local name = vim.fn.expand "%:t:r"
+  if name == "" then
+    return
+  end
+  builtin.live_grep { default_text = "(" .. name .. ")" }
+end, { desc = "Search '(filename)' (no ext)" })
