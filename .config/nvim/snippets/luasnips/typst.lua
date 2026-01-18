af560ca9afcf98f7d66f4a96b5cab725aad6a048
Beed <annoor28@hotmail.co.uk>
2026-01-18 00:43:27 +0000

    chore: Better typst snippet

--- .config/nvim/snippets/luasnips/typst.lua
+++ .config/nvim/snippets/luasnips/typst.lua
@@ -3,20 +3,55 @@ local ls = require "luasnip"
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local f = ls.function_node
+
+local function id_from_filename()
+  local name = vim.fn.expand "%:t:r"
+  -- extract YYYYMMDDHHMM
+  return name:match "^(%d%d%d%d%d%d%d%d%d%d%d%d)" or ""
+end
+
+local function title_from_filename()
+  local filename = vim.fn.expand "%:t:r"
+
+  -- remove leading YYYYMMDDHHMM + optional separator
+  filename = filename:gsub("^%d%d%d%d%d%d%d%d%d%d%d%d[-_]?", "")
+
+  filename = filename:gsub("_", " ")
+
+  filename = filename:gsub("(%a)([%w']*)", function(first, rest)
+    return first:upper() .. rest:lower()
+  end)
+
+  return filename
+end
 
 ls.add_snippets("typst", {
-  s("zn", {
+  s("main", {
     t {
       '#import "style.typ": style',
       "#show: style",
       "",
-      "= ",
+      "",
     },
-    i(1, "Title"),
+    t "ID = ",
+    f(id_from_filename),
     t {
       "",
       "",
+      ":main:",
     },
+    i(1),
+    t {
+      "",
+      "",
+      "= ",
+    },
+    f(title_from_filename),
+    t { "", "" },
     i(2),
+    t { "", "", "= Links", "" },
+    i(3),
+    t { "", "", '#bibliography("sources.yaml")' },
   }),
 })
