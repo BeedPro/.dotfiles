f83d003a0845a899b6a096e4f9b5426f50894c60
Beed <annoor28@hotmail.co.uk>
2026-01-30 21:22:56 +0000

    Add some change theme switching

--- .config/fzf/dark.sh
+++ .config/fzf/dark.sh
@@ -4,3 +4,5 @@ export FZF_DEFAULT_OPTS=" \
 --color=marker:#8a9a7b,fg+:#C5C9C7,prompt:#8ba4b0,hl+:#e46876 \
 --color=selected-bg:#393B44 \
 --color=border:#A4A7A4,label:#C5C9C7"
+
+tmux set-environment -g FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS" 2>/dev/null
