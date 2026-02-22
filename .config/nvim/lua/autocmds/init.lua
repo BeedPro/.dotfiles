984280f7ac23128b54c845cf22b71c7628edc9e9
Beed <annoor28@hotmail.co.uk>
2026-02-22 14:35:09 +0000

    remove signature.lua

--- .config/nvim/lua/autocmds/init.lua
+++ .config/nvim/lua/autocmds/init.lua
@@ -3,7 +3,6 @@ local autocmd = vim.api.nvim_create_autocmd
 require "autocmds.ready"
 require "autocmds.numbertoggle"
 require "autocmds.conform"
-require "autocmds.signature"
 require "autocmds.typst"
 require "autocmds.django"
 require "autocmds.oil"
