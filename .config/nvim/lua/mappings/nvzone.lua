8c43dc2913e81dab20c64d267a3b4e0748c4b81e
Beed <annoor28@hotmail.co.uk>
2026-02-22 14:45:24 +0000

    chore: update nvim to remove nvzone

--- .config/nvim/lua/mappings/nvzone.lua
+++
@@ -1,9 +0,0 @@
-local map = vim.keymap.set
-
-map({ "n", "v" }, "<RightMouse>", function()
-  require("menu.utils").delete_old_menus()
-
-  vim.cmd.exec '"normal! \\<RightMouse>"'
-
-  require("menu").open("default", { mouse = true })
-end, {})
