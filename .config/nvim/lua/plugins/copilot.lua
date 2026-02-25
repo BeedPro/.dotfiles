eccf816e87d5df4482b6218a89b4e18309c07112
Beed <annoor28@hotmail.co.uk>
2026-02-25 23:09:57 +0000

    Remove copilot

--- .config/nvim/lua/plugins/copilot.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "github/copilot.vim",
-  event = "InsertEnter",
-  init = function()
-    vim.g.copilot_no_maps = true
-    vim.g.copilot_no_tab_map = true
-    vim.g.copilot_hide_during_completion = true
-  end,
-}
