3e31103aeadadbf4bca2cbe10cc356640ae4b5b4
BeedPro <annoor28@hotmail.co.uk>
2026-03-05 23:41:44 +0000

    feat: add gh to nvim

--- .config/nvim/lua/mappings/snacks.lua
+++ .config/nvim/lua/mappings/snacks.lua
@@ -9,3 +9,19 @@ end, { desc = "[N]otification History" })
 map("n", "<leader>un", function()
   snacks.notifier.hide()
 end, { desc = "[U][N]otifications" })
+
+map("n", "<leader>gi", function()
+  snacks.picker.gh_issue()
+end, { desc = "[G]itHub [I]ssues (open)" })
+
+map("n", "<leader>gI", function()
+  snacks.picker.gh_issue { state = "all" }
+end, { desc = "[G]itHub [I]ssues (all)" })
+
+map("n", "<leader>gp", function()
+  snacks.picker.gh_pr()
+end, { desc = "[G]itHub [P]ull Requests (open)" })
+
+map("n", "<leader>gP", function()
+  snacks.picker.gh_pr { state = "all" }
+end, { desc = "[G]itHub [P]ull Requests (all)" })
