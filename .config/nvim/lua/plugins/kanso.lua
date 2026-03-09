1205bd0c59306dd4ba70c3c0c96c2db48ed54382
BeedPro <annoor28@hotmail.co.uk>
2026-03-09 16:15:39 +0000

    feat: remove underlines and undercurls to kanso.nvim

--- .config/nvim/lua/plugins/kanso.lua
+++ .config/nvim/lua/plugins/kanso.lua
@@ -12,11 +12,37 @@ return {
       },
       overrides = function(colors)
         return {
-          LspReferenceWrite = { underline = false },
+          -- treesitter
+          ["@markup.underline"] = { underline = false, undercurl = false },
+          ["@string.special.url"] = { underline = false, undercurl = false },
+
+          -- builtin
+          Underlined = { underline = false, undercurl = false },
+
+          -- spell
+          SpellBad = { underline = false, undercurl = false },
+          SpellCap = { underline = false, undercurl = false },
+          SpellLocal = { underline = false, undercurl = false },
+          SpellRare = { underline = false, undercurl = false },
+
+          -- lsp
+          LspReferenceWrite = { underline = false, undercurl = false },
+
+          -- plugins
+          IndentBlanklineContextStart = { underline = false, undercurl = false },
+          ["@ibl.scope.underline.1"] = { underline = false, undercurl = false },
+
+          MiniCompletionActiveParameter = { underline = false, undercurl = false },
+          MiniCursorword = { underline = false, undercurl = false },
+          MiniCursorwordCurrent = { underline = false, undercurl = false },
+
+          NeotestFocused = { underline = false, undercurl = false },
+
           WinSeparator = { fg = colors.palette.gray3 },
         }
       end,
     }
+
     vim.cmd "colorscheme kanso"
   end,
 }
