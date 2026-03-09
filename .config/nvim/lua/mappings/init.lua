04dbef59e8c73644a53a2c83ef81fc6c75f88970
BeedPro <annoor28@hotmail.co.uk>
2026-03-09 19:09:46 +0000

    feat: remove g? to use default <C-w>d

--- .config/nvim/lua/mappings/init.lua
+++ .config/nvim/lua/mappings/init.lua
@@ -10,7 +10,3 @@ local map = vim.keymap.set
 
 map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "[D]iagnostic [S]how (loclist)" })
 map("n", "<leader>da", vim.diagnostic.setqflist, { desc = "[D]iagnostic [A]all (globallist)" })
-
-map("n", "g?", function()
-  vim.diagnostic.open_float(nil, { border = "single", source = "always" })
-end, { desc = "[D]iagnostic [H]over" })
