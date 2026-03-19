83e6c9dd1d2d2da72a4363c59a5d08ce091f1dda
BeedPro <annoor28@hotmail.co.uk>
2026-03-19 19:14:14 +0000

    refactor: replace snacks bigfile with native autocmd

--- .config/nvim/lua/autocmds/init.lua
+++ .config/nvim/lua/autocmds/init.lua
@@ -3,6 +3,7 @@ local autocmd = vim.api.nvim_create_autocmd
 require "autocmds.ready"
 require "autocmds.numbertoggle"
 require "autocmds.conform"
+require "autocmds.bigfile"
 require "autocmds.typst"
 require "autocmds.django"
 require "autocmds.oil"
