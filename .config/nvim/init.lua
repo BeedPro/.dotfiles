02c1e715b3a1d3db165abffb628fa77db5370528
BeedPro <annoor28@hotmail.co.uk>
2026-03-22 08:35:34 +0000

    feat: remove the space in init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,7 +4,6 @@ vim.g.lua_snippets_path = vim.fn.stdpath "config" .. "/snippets/luasnips"
 vim.g.vscode_snippets_path = "./snippets/vscode"
 
 require "plugins"
-
 require "options"
 require "autocmds"
 require "commands"
