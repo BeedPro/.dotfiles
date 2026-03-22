9d09243a2fe797e934626d192fe2399afe62ed79
BeedPro <annoor28@hotmail.co.uk>
2026-03-22 07:38:04 +0000

    feat(dap): add luajit launch profile for lua files

--- .config/nvim/lua/configs/dap/configurations/lua.lua
+++ .config/nvim/lua/configs/dap/configurations/lua.lua
@@ -10,4 +10,15 @@ return {
     },
     args = {},
   },
+  {
+    name = "Current file (local-lua-dbg, luajit)",
+    type = "local-lua",
+    request = "launch",
+    cwd = "${workspaceFolder}",
+    program = {
+      lua = "luajit",
+      file = "${file}",
+    },
+    args = {},
+  },
 }
