271b925e13604f9c8e796c46445d9b8b781ddfab
BeedPro <annoor28@hotmail.co.uk>
2026-04-19 16:16:48 +0100

    feat: remove firefox/chrome

--- .local/scripts/genapps.sh
+++ .local/scripts/genapps.sh
@@ -9,6 +9,8 @@ TERMINAL=alacritty
 
 APPS=(
   "Godot|godot"
+  "Neovide|neovide"
+  "Chrome|chrome"
   "Chat|$HOME/.local/scripts/openapps chat"
   "Email|$HOME/.local/scripts/openapps email"
   "Bookmarks|$HOME/.local/scripts/openbms"
