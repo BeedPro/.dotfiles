e0f4e1b84b076f3ae6e21e66f309c66680a9e428
BeedPro <annoor28@hotmail.co.uk>
2026-04-22 17:23:17 +0100

    feat: update genapp.sh

--- .local/scripts/genapps.sh
+++ .local/scripts/genapps.sh
@@ -9,7 +9,7 @@ TERMINAL=alacritty
 
 APPS=(
   "Godot|godot"
-  "Chromium|env GOOGLE_API_KEY=no GOOGLE_DEFAULT_CLIENT_ID=no GOOGLE_DEFAULT_CLIENT_SECRET=no chromium"
+  "Chromium|env GOOGLE_API_KEY=no GOOGLE_DEFAULT_CLIENT_ID=no GOOGLE_DEFAULT_CLIENT_SECRET=no chromium --disable-features=ExtensionManifestV2Unsupported"
   "Helium|helium"
   "Chat|$HOME/.local/scripts/openapps chat"
   "Email|$HOME/.local/scripts/openapps email"
