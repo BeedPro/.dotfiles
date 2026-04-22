a8f2a2c400f56b813d4442c787ae645221cd1a77
BeedPro <annoor28@hotmail.co.uk>
2026-04-22 17:59:48 +0100

    feat: use ublock origin lite for chromium

--- .local/scripts/genapps.sh
+++ .local/scripts/genapps.sh
@@ -9,7 +9,7 @@ TERMINAL=alacritty
 
 APPS=(
   "Godot|godot"
-  "Chromium|env GOOGLE_API_KEY=no GOOGLE_DEFAULT_CLIENT_ID=no GOOGLE_DEFAULT_CLIENT_SECRET=no chromium --disable-features=ExtensionManifestV2Unsupported"
+  "Chromium|env GOOGLE_API_KEY=no GOOGLE_DEFAULT_CLIENT_ID=no GOOGLE_DEFAULT_CLIENT_SECRET=no chromium"
   "Helium|helium"
   "Chat|$HOME/.local/scripts/openapps chat"
   "Email|$HOME/.local/scripts/openapps email"
