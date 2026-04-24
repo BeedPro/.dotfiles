cd5c7d010624021d309a50dbf1719f7125adbd29
BeedPro <annoor28@hotmail.co.uk>
2026-04-24 18:40:51 +0100

    feat: add snipmate to snippets

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 vim.g.lua_snippets_path = vim.fs.joinpath(vim.fn.stdpath "config", "snippets", "luasnips")
+vim.g.snipmate_snippets_path = vim.fs.joinpath(vim.fn.stdpath "config", "snippets", "snipmate")
 vim.g.vscode_snippets_path = vim.fs.joinpath(vim.fn.stdpath "config", "snippets", "vscode")
 
 require "plugins"
