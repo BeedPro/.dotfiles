46f2dc312d4a0ed4eeae0ef929e0793322d96650
BeedPro <annoor28@hotmail.co.uk>
2026-04-24 18:42:24 +0100

    feat: update lua/configs/luasnip.lua

--- .config/nvim/lua/configs/luasnip.lua
+++ .config/nvim/lua/configs/luasnip.lua
@@ -1,27 +1,40 @@
-local opts = { history = true, updateevents = "TextChanged,TextChangedI" }
+local ls = require "luasnip"
 
-require("luasnip").config.set_config(opts)
+ls.config.set_config { history = true, updateevents = "TextChanged,TextChangedI" }
 
 -- vscode format
-require("luasnip.loaders.from_vscode").lazy_load { exclude = vim.g.vscode_snippets_exclude or {} }
-require("luasnip.loaders.from_vscode").lazy_load { paths = vim.g.vscode_snippets_path or "" }
+require("luasnip.loaders.from_vscode").lazy_load {
+  exclude = vim.g.vscode_snippets_exclude or {},
+  fs_event_providers = { autocmd = true, libuv = true },
+}
 
--- snipmate format
-require("luasnip.loaders.from_snipmate").load()
-require("luasnip.loaders.from_snipmate").lazy_load { paths = vim.g.snipmate_snippets_path or "" }
+require("luasnip.loaders.from_vscode").lazy_load {
+  paths = { vim.g.vscode_snippets_path },
+  fs_event_providers = { autocmd = true, libuv = true },
+}
+
+-- snipmate format (enable if you use snipmate snippets)
+if vim.g.snipmate_snippets_path then
+  require("luasnip.loaders.from_snipmate").lazy_load {
+    paths = { vim.g.snipmate_snippets_path },
+    fs_event_providers = { autocmd = true, libuv = true },
+  }
+end
 
 -- lua format
-require("luasnip.loaders.from_lua").load()
-require("luasnip.loaders.from_lua").lazy_load { paths = vim.g.lua_snippets_path or "" }
+require("luasnip.loaders.from_lua").lazy_load {
+  paths = { vim.g.lua_snippets_path },
+  fs_event_providers = { autocmd = true, libuv = true },
+}
 
 -- fix luasnip #258
 vim.api.nvim_create_autocmd("InsertLeave", {
   callback = function()
     if
-      require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
-      and not require("luasnip").session.jump_active
+      ls.session.current_nodes[vim.api.nvim_get_current_buf()]
+      and not ls.session.jump_active
     then
-      require("luasnip").unlink_current()
+      ls.unlink_current()
     end
   end,
 })
