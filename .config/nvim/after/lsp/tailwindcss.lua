277938b6aaab9562b4f03429aca2ab4cd323b224
BeedPro <annoor28@hotmail.co.uk>
2026-04-24 20:21:00 +0100

    feat: override tailwindcss filetypes with after/lsp config

---
+++ .config/nvim/after/lsp/tailwindcss.lua
@@ -0,0 +1,17 @@
+---@type vim.lsp.Config
+return {
+  filetypes = {
+    "astro",
+    "css",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+    "vue",
+    "svelte",
+    "scss",
+    "sass",
+    "less",
+  },
+}
