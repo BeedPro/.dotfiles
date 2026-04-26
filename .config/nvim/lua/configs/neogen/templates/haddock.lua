9ba4a83ab1b7a05f03976f521ba1eeba118311d0
BeedPro <annoor28@hotmail.co.uk>
2026-04-26 17:30:51 +0100

    feat: add haskell neogen doc template

---
+++ .config/nvim/lua/configs/neogen/templates/haddock.lua
@@ -0,0 +1,10 @@
+local i = require("neogen.types.template").item
+
+return {
+  { nil, "-- | $1", { no_results = true, type = { "func" } } },
+  { nil, "--", { no_results = true, type = { "func" } } },
+  { nil, "-- | $1" },
+  { nil, "--" },
+  { i.Parameter, "-- @param %s $1", { type = { "func" } } },
+  { i.Return, "-- @return $1", { type = { "func" } } },
+}
