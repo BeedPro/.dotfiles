f0de0f7de3e385a6e6a2ec9946fce9a45accdb9c
BeedPro <annoor28@hotmail.co.uk>
2026-04-26 17:38:10 +0100

    fix: remove duplicate haddock separator line

--- .config/nvim/lua/configs/neogen/templates/haddock.lua
+++ .config/nvim/lua/configs/neogen/templates/haddock.lua
@@ -2,7 +2,6 @@ local i = require("neogen.types.template").item
 
 return {
   { nil, "-- | $1", { no_results = true, type = { "func" } } },
-  { nil, "--", { no_results = true, type = { "func" } } },
   { nil, "-- | $1" },
   { nil, "--" },
   { i.Parameter, "-- @param %s $1", { type = { "func" } } },
