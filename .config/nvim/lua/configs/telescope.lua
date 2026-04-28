4b11683fd32c3c477435255105d571e68ba07ad6
BeedPro <annoor28@hotmail.co.uk>
2026-04-29 00:27:59 +0100

    feat: add a space to the icons on telescope

--- .config/nvim/lua/configs/telescope.lua
+++ .config/nvim/lua/configs/telescope.lua
@@ -2,6 +2,8 @@ local actions = require "telescope.actions"
 
 return {
   defaults = {
+    selection_caret = " ",
+    entry_prefix = " ",
     sorting_strategy = "ascending",
 
     border = true,
