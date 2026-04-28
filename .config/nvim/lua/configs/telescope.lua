35a7c84e9fce91849aac47a009b67c4424c43374
BeedPro <annoor28@hotmail.co.uk>
2026-04-29 00:25:53 +0100

    feat: revert to more default telescope behavour

--- .config/nvim/lua/configs/telescope.lua
+++ .config/nvim/lua/configs/telescope.lua
@@ -2,8 +2,6 @@ local actions = require "telescope.actions"
 
 return {
   defaults = {
-    selection_caret = " ",
-    entry_prefix = " ",
     sorting_strategy = "ascending",
 
     border = true,
