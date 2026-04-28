034693092821b2309a1c75516ec269313463a666
BeedPro <annoor28@hotmail.co.uk>
2026-04-29 00:23:13 +0100

    feat: revert the telescope to be top

--- .config/nvim/lua/configs/telescope.lua
+++ .config/nvim/lua/configs/telescope.lua
@@ -16,10 +16,7 @@ return {
     layout_config = {
       horizontal = {
         prompt_position = "top",
-        preview_width = 0.55,
       },
-      width = 0.87,
-      height = 0.80,
     },
 
     mappings = {
