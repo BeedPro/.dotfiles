3c549ce095bb1253d2432b57ab8c57c292433278
BeedPro <annoor28@hotmail.co.uk>
2026-04-28 22:37:39 +0100

    feat: update blink keymaps

--- .config/nvim/lua/configs/blink.lua
+++ .config/nvim/lua/configs/blink.lua
@@ -13,13 +13,11 @@ return {
     ["<C-e>"] = { "hide", "fallback" },
     ["<C-y>"] = { "accept", "fallback" },
 
-    ["<Up>"] = { "select_prev", "fallback" },
-    ["<Down>"] = { "select_next", "fallback" },
     ["<C-n>"] = { "select_next", "fallback" },
     ["<C-p>"] = { "select_prev", "fallback" },
 
-    ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-    ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+    ["<Up>"] = { "scroll_documentation_up", "fallback" },
+    ["<Down>"] = { "scroll_documentation_down", "fallback" },
 
     ["<C-j>"] = { "snippet_forward", "fallback" },
     ["<C-k>"] = { "snippet_backward", "fallback" },
