576dd44a976c413a0ce453fa619e506b86d5861b
Beed <annoor28@hotmail.co.uk>
2026-04-30 04:19:24 +0100

    refactor: inline lsp attach keymap options

--- .config/nvim/lua/configs/lsp/attach.lua
+++ .config/nvim/lua/configs/lsp/attach.lua
@@ -4,16 +4,12 @@ local telescope = require "telescope.builtin"
 
 -- export on_attach & capabilities
 M.on_attach = function(_, bufnr)
-  local function opts(desc)
-    return { buffer = bufnr, desc = desc }
-  end
-
-  map("n", "grr", telescope.lsp_references, opts "[C]ode [R]eferences")
-  map("n", "gri", telescope.lsp_implementations, opts "[C]ode [I]mplementation")
-  map("n", "grt", telescope.lsp_type_definitions, opts "[C]ode [T]ype Definition")
-  map("n", "gO", telescope.lsp_document_symbols, opts "[C]ode Symb[O]ls")
-  map("n", "gD", vim.lsp.buf.declaration, opts "[G]oto [D]eclaration")
-  map("n", "gd", telescope.lsp_definitions, opts "[G]oto [d]efinition")
+  map("n", "grr", telescope.lsp_references, { buffer = bufnr, desc = "[C]ode [R]eferences" })
+  map("n", "gri", telescope.lsp_implementations, { buffer = bufnr, desc = "[C]ode [I]mplementation" })
+  map("n", "grt", telescope.lsp_type_definitions, { buffer = bufnr, desc = "[C]ode [T]ype Definition" })
+  map("n", "gO", telescope.lsp_document_symbols, { buffer = bufnr, desc = "[C]ode Symb[O]ls" })
+  map("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "[G]oto [D]eclaration" })
+  map("n", "gd", telescope.lsp_definitions, { buffer = bufnr, desc = "[G]oto [d]efinition" })
 end
 
 return M
