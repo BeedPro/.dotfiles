89df5c4aa6c3562b14f6e59dc38a8ab183361824
Beed <annoor28@hotmail.co.uk>
2026-04-30 04:48:06 +0100

    feat: move the snippet variables to e local

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,4 @@
 vim.g.mapleader = " "
-vim.g.lua_snippets_path = vim.fs.joinpath(vim.fn.stdpath "config", "snippets", "luasnips")
-vim.g.snipmate_snippets_path = vim.fs.joinpath(vim.fn.stdpath "config", "snippets", "snipmate")
-vim.g.vscode_snippets_path = vim.fs.joinpath(vim.fn.stdpath "config", "snippets", "vscode")
 
 require "plugins"
 require "options"
--- .config/nvim/lua/configs/luasnip.lua
+++ .config/nvim/lua/configs/luasnip.lua
@@ -1,25 +1,31 @@
 local ls = require "luasnip"
+local config_path = vim.fn.stdpath "config"
+local lua_snippets_path = vim.fs.joinpath(config_path, "snippets", "luasnips")
+local snipmate_snippets_path = vim.fs.joinpath(config_path, "snippets", "snipmate")
+local vscode_snippets_path = vim.fs.joinpath(config_path, "snippets", "vscode")
+
+local vscode_snippets_exclude = {}
 
 ls.config.set_config { history = true, updateevents = "TextChanged,TextChangedI" }
 
 require("luasnip.loaders.from_vscode").lazy_load {
-  exclude = vim.g.vscode_snippets_exclude or {},
+  exclude = vscode_snippets_exclude,
   fs_event_providers = { autocmd = true, libuv = true },
 }
 
 require("luasnip.loaders.from_vscode").lazy_load {
-  paths = { vim.g.vscode_snippets_path },
+  paths = { vscode_snippets_path },
   fs_event_providers = { autocmd = true, libuv = true },
 }
 
-if vim.g.snipmate_snippets_path then
+if snipmate_snippets_path then
   require("luasnip.loaders.from_snipmate").lazy_load {
-    paths = { vim.g.snipmate_snippets_path },
+    paths = { snipmate_snippets_path },
     fs_event_providers = { autocmd = true, libuv = true },
   }
 end
 
 require("luasnip.loaders.from_lua").lazy_load {
-  paths = { vim.g.lua_snippets_path },
+  paths = { lua_snippets_path },
   fs_event_providers = { autocmd = true, libuv = true },
 }
