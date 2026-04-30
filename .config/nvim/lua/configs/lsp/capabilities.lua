3462cf2af3e5e6e63a2f4bb655694c7816457f82
Beed <annoor28@hotmail.co.uk>
2026-04-30 04:14:53 +0100

    refactor: remove default LSP capability overrides

--- .config/nvim/lua/configs/lsp/capabilities.lua
+++ .config/nvim/lua/configs/lsp/capabilities.lua
@@ -3,14 +3,8 @@ local M = {}
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
 
 M.capabilities.textDocument.completion.completionItem = {
-  documentationFormat = { "markdown", "plaintext" },
-  snippetSupport = true,
   preselectSupport = true,
-  insertReplaceSupport = true,
-  labelDetailsSupport = true,
-  deprecatedSupport = true,
   commitCharactersSupport = true,
-  tagSupport = { valueSet = { 1 } },
   resolveSupport = {
     properties = {
       "documentation",
