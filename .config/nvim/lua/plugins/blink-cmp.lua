4f7fafa6a4d7ffca593585d082874cf44ee2010a
BeedPro <annoor28@hotmail.co.uk>
2026-05-01 00:54:44 +0100

    feat: remove dependency from blink-cmp-spell

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -2,9 +2,6 @@ return {
   "saghen/blink.cmp",
   version = "1.*",
   event = { "InsertEnter", "CmdLineEnter" },
-  dependencies = { "ribru17/blink-cmp-spell" },
-
   opts_extend = { "sources.default" },
-
   opts = require "configs.blink-cmp",
 }
