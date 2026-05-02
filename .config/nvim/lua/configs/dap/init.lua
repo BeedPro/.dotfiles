b73f252c4c70bbd2301373a146fa018a6ac8592e
BeedPro <annoor28@hotmail.co.uk>
2026-05-02 22:13:58 +0100

    feat: remove lua from dap

--- .config/nvim/lua/configs/dap/init.lua
+++ .config/nvim/lua/configs/dap/init.lua
@@ -3,7 +3,6 @@ local dap = require "dap"
 require "configs.dap.adapters.python"
 require "configs.dap.adapters.codelldb"
 require "configs.dap.adapters.haskell"
-require "configs.dap.adapters.local_lua"
 require "configs.dap.adapters.javascript"
 require "configs.dap.adapters.firefox"
 require "configs.dap.adapters.godot"
@@ -14,7 +13,6 @@ dap.configurations.python = require "configs.dap.configurations.python"
 dap.configurations.cpp = require "configs.dap.configurations.cpp"
 dap.configurations.c = dap.configurations.cpp
 dap.configurations.haskell = require "configs.dap.configurations.haskell"
-dap.configurations.lua = require "configs.dap.configurations.lua"
 dap.configurations.javascript = require "configs.dap.configurations.javascript"
 dap.configurations.javascriptreact = dap.configurations.javascript
 dap.configurations.typescript = dap.configurations.javascript
