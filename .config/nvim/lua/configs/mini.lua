17e5e124f30bc61bec24577c359c9ce0c3c970ca
BeedPro <annoor28@hotmail.co.uk>
2026-05-05 04:07:32 +0100

    feat: add hipatterns

--- .config/nvim/lua/configs/mini.lua
+++ .config/nvim/lua/configs/mini.lua
@@ -2,3 +2,14 @@ local sessions = require "mini.sessions"
 sessions.setup {
   force = { read = false, write = true, delete = true },
 }
+
+local hipatterns = require "mini.hipatterns"
+hipatterns.setup {
+  highlighters = {
+    fixme = { pattern = "FIXME", group = "MiniHipatternsFixme" },
+    hack = { pattern = "HACK", group = "MiniHipatternsHack" },
+    todo = { pattern = "TODO", group = "MiniHipatternsTodo" },
+    note = { pattern = "NOTE", group = "MiniHipatternsNote" },
+    hex_color = hipatterns.gen_highlighter.hex_color(),
+  },
+}
