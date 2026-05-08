7226c171b2bad1aa42845753cc12293eee0dd27e
BeedPro <annoor28@hotmail.co.uk>
2026-05-08 23:56:31 +0100

    feat: make the todos be on word

--- .config/nvim/lua/configs/mini.lua
+++ .config/nvim/lua/configs/mini.lua
@@ -6,10 +6,10 @@ sessions.setup {
 local hipatterns = require "mini.hipatterns"
 hipatterns.setup {
   highlighters = {
-    fixme = { pattern = "FIXME", group = "MiniHipatternsFixme" },
-    hack = { pattern = "HACK", group = "MiniHipatternsHack" },
-    todo = { pattern = "TODO", group = "MiniHipatternsTodo" },
-    note = { pattern = "NOTE", group = "MiniHipatternsNote" },
+    fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+    hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+    todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+    note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
     hex_color = hipatterns.gen_highlighter.hex_color(),
   },
 }
