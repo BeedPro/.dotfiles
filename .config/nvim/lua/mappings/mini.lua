bfa3010a071d8a3f2bec3bab639f018fbc63b03f
BeedPro <annoor28@hotmail.co.uk>
2026-05-09 23:07:18 +0100

    feat: sd and so

--- .config/nvim/lua/mappings/mini.lua
+++ .config/nvim/lua/mappings/mini.lua
@@ -1,9 +1,9 @@
 local map = vim.keymap.set
 
-map("n", "<leader>mso", function()
+map("n", "<leader>so", function()
   MiniSessions.read()
 end, { desc = "[M]ini [S][O]urce Session" })
 
-map("n", "<leader>mds", function()
+map("n", "<leader>sd", function()
   MiniSessions.delete()
 end, { desc = "[M]ini [D]elete [S]ession" })
