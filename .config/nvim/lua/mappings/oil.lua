b1eb554944e1bf8400ce3c956028fe83c41e0bec
BeedPro <annoor28@hotmail.co.uk>
2026-05-09 23:15:45 +0100

    feat: add find files in oil

--- .config/nvim/lua/mappings/oil.lua
+++ .config/nvim/lua/mappings/oil.lua
@@ -24,5 +24,11 @@ autocmd("FileType", {
         require("oil").set_columns {}
       end
     end, { buffer = args.buf, desc = "Toggle file detail view" })
+
+    map("n", "<leader>ff", function()
+      require("fzf-lua").files {
+        cwd = require("oil").get_current_dir(),
+      }
+    end, { buffer = args.buf, desc = "Find files in the current directory" })
   end,
 })
