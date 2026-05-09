26226f1c80661918d0a8688d4f6b7a572f66c32d
BeedPro <annoor28@hotmail.co.uk>
2026-05-09 23:07:51 +0100

    feat: remove delete

--- .config/nvim/lua/mappings/mini.lua
+++ .config/nvim/lua/mappings/mini.lua
@@ -3,7 +3,3 @@ local map = vim.keymap.set
 map("n", "<leader>so", function()
   MiniSessions.read()
 end, { desc = "[M]ini [S][O]urce Session" })
-
-map("n", "<leader>sd", function()
-  MiniSessions.delete()
-end, { desc = "[M]ini [D]elete [S]ession" })
