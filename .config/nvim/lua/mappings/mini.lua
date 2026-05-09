e55266be52fa037f2325a28d86d99081c12341f4
BeedPro <annoor28@hotmail.co.uk>
2026-05-09 23:02:13 +0100

    feat: remove global sessions

--- .config/nvim/lua/mappings/mini.lua
+++ .config/nvim/lua/mappings/mini.lua
@@ -1,19 +1,9 @@
 local map = vim.keymap.set
 
-map("n", "<leader>mks", function()
-  vim.ui.input({ prompt = "Session name: " }, function(input)
-    if input == nil or input == "" then
-      return
-    end
-
-    MiniSessions.write(input)
-  end)
-end, { desc = "[M]ini add [S]ession" })
-
 map("n", "<leader>mso", function()
-  MiniSessions.select "read"
+  MiniSessions.read()
 end, { desc = "[M]ini [S][O]urce Session" })
 
 map("n", "<leader>mds", function()
-  MiniSessions.select "delete"
+  MiniSessions.delete()
 end, { desc = "[M]ini [D]elete [S]ession" })
