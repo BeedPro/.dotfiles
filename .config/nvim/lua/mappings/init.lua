a00acf03a44aad5565d177a0bd72b92d0acdfb9d
BeedPro <annoor28@hotmail.co.uk>
2026-05-09 23:20:21 +0100

    feat: remove a non-default

--- .config/nvim/lua/mappings/init.lua
+++ .config/nvim/lua/mappings/init.lua
@@ -10,4 +10,3 @@ local map = vim.keymap.set
 
 map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "[D]iagnostic [S]how (loclist)" })
 map("n", "<leader>da", vim.diagnostic.setqflist, { desc = "[D]iagnostic [A]ll (quickfix)" })
-map("t", "<Esc>", "<C-\\><C-n>", { desc = "[T]erminal: [E]xit to Normal Mode" })
