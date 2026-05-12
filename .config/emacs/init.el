5ba4d04aed47941edde818d93835853a83f79514
BeedPro <annoor28@hotmail.co.uk>
2026-05-12 15:27:39 +0100

    feat: add minimal emacs config

---
+++ .config/emacs/init.el
@@ -0,0 +1,38 @@
+;; -*- lexical-binding: t; -*-
+(setq inhibit-startup-message t
+      auto-save-default nil
+      make-backup-files nil
+      set-mark-command-repeat-pop t
+      large-file-warning-threshold nil
+      vc-follow-symlinks t
+      ad-redefinition-action 'accept
+      use-package-always-ensure t
+      global-auto-revert-non-file-buffers t
+      native-comp-async-report-warnings-errors nil)
+
+(repeat-mode 1)
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(savehist-mode 1)
+(scroll-bar-mode 0)
+(xterm-mouse-mode 1)
+(display-time-mode 1)
+(column-number-mode 1)
+(tab-bar-history-mode 1)
+(auto-save-visited-mode 1)
+(global-visual-line-mode 1)
+(global-auto-revert-mode 1)
+
+(setq-default indent-tabs-mode nil
+              tab-width 2
+              display-line-numbers-type 'relative)
+
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file t))
+
+(load-theme 'wombat)
