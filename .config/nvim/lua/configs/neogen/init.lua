cbe88db19d4183471889b23af3f866c0aa047d79
BeedPro <annoor28@hotmail.co.uk>
2026-05-14 17:19:49 +0100

    feat: remove haddock from neogen

--- .config/nvim/lua/configs/neogen/init.lua
+++ .config/nvim/lua/configs/neogen/init.lua
@@ -1,27 +1,6 @@
 return {
   snippet_engine = "luasnip",
   languages = {
-    haskell = {
-      parent = {
-        func = { "function", "bind", "signature" },
-      },
-      data = {
-        func = {
-          ["function|bind|signature"] = {
-            ["0"] = {
-              extract = function()
-                return {}
-              end,
-            },
-          },
-        },
-      },
-      template = {
-        annotation_convention = "haddock",
-        use_default_comment = false,
-        haddock = require "configs.neogen.templates.haddock",
-      },
-    },
     python = {
       template = {
         annotation_convention = "google_docstrings",
--- .config/nvim/lua/configs/neogen/templates/haddock.lua
+++
@@ -1,9 +0,0 @@
-local i = require("neogen.types.template").item
-
-return {
-  { nil, "-- | $1", { no_results = true, type = { "func" } } },
-  { nil, "-- | $1" },
-  { nil, "--" },
-  { i.Parameter, "-- @param %s $1", { type = { "func" } } },
-  { i.Return, "-- @return $1", { type = { "func" } } },
-}
