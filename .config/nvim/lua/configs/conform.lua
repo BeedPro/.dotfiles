3d46a2dfd8ded84f3061185cd9c36bbf2c242782
BeedPro <annoor28@hotmail.co.uk>
2026-05-14 17:06:44 +0100

    feat: disable format on save

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -22,12 +22,7 @@ local options = {
     ["_"] = { "trim_whitespace" },
   },
 
-  format_on_save = function(bufnr)
-    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-      return
-    end
-    return { timeout_ms = 500, lsp_format = "fallback" }
-  end,
+  format_on_save = nil,
 }
 
 return options
