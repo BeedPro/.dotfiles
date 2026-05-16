d212c01e7e7859a29fcdf598be5adadc70744349
BeedPro <annoor28@hotmail.co.uk>
2026-05-16 14:35:40 +0100

    feat: add modus colors

---
+++ .config/fzf/colors/modus-light.sh
@@ -0,0 +1,8 @@
+export FZF_DEFAULT_OPTS=" \
+--color=bg:#f2f1ef,bg+:#c9cbd1,spinner:#e98a00,hl:#c84053 \
+--color=fg:#545464,header:#4d699b,info:#597b75,pointer:#b35b79 \
+--color=marker:#6f894e,fg+:#14171d,prompt:#4d699b,hl+:#d7474b \
+--color=selected-bg:#c9cbd1 \
+--color=border:#8a8980,label:#545464"
+
+tmux set-environment -g FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS" 2>/dev/null
