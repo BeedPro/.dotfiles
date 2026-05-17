29432b7b79d6da4772c7b189973c3728a8c5ddac
BeedPro <annoor28@hotmail.co.uk>
2026-05-17 15:20:43 +0100

    feat: add vundo tree and diff-hl to emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -141,3 +141,19 @@
   :bind ("C-x g" . magit-status)
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package vundo
+  :bind ("C-x u" . vundo))
+
+(use-package diff-hl
+  :defer nil
+  :hook ((prog-mode         . diff-hl-mode)
+         (text-mode         . diff-hl-mode)
+         (dired-mode        . diff-hl-dired-mode)
+         (magit-pre-refresh . diff-hl-magit-pre-refresh)
+         (magit-post-refresh . diff-hl-magit-post-refresh))
+  :config
+  (global-diff-hl-mode 1)
+  (unless (display-graphic-p)
+    (diff-hl-margin-mode 1))
+  (diff-hl-flydiff-mode 1))
