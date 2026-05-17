057827a49469c6ef67206f13168ce7531cd0505a
BeedPro <annoor28@hotmail.co.uk>
2026-05-17 22:10:57 +0100

    feat: add keybinds to switch arabic modes

--- .config/nvim/lua/mappings/init.lua
+++ .config/nvim/lua/mappings/init.lua
@@ -10,3 +10,19 @@ local map = vim.keymap.set
 
 map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "[D]iagnostic [S]how (loclist)" })
 map("n", "<leader>da", vim.diagnostic.setqflist, { desc = "[D]iagnostic [A]ll (quickfix)" })
+map("n", "<leader>ta", function()
+  if vim.wo.arabic then
+    vim.cmd "set noarab"
+    return
+  end
+
+  vim.cmd "set arab"
+end, { desc = "[T]oggle [A]rabic" })
+
+map("i", "<C-^>", function()
+  if vim.wo.arabic then
+    return vim.api.nvim_replace_termcodes("<C-o>:set noarab<CR>", true, false, true)
+  end
+
+  return vim.api.nvim_replace_termcodes("<C-o>:set arab<CR>", true, false, true)
+end, { expr = true, desc = "Toggle arabic" })
