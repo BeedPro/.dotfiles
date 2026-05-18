aedf25406b5db1b1a3ebb16318ddd3ab0e8457f1
BeedPro <annoor28@hotmail.co.uk>
2026-05-18 18:12:52 +0100

    feat: have default listchars on nvim

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,6 +6,11 @@ o.spellfile = vim.fs.joinpath(vim.fn.stdpath "config", "spell", "en.utf-8.add")
 
 
 o.list = true
+opt.listchars = {
+  tab = '> ',
+  trail = '-',
+  nbsp = '+',
+}
 o.background = "dark"
 o.winborder = "single"
 o.laststatus = 3
