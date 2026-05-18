054949a505b143e9e23125218c459f670b637302
BeedPro <annoor28@hotmail.co.uk>
2026-05-18 15:59:25 +0100

    feat: add consult, orderless, and embark

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -194,6 +194,33 @@
   :init
   (marginalia-mode 1))
 
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides
+   '((file (styles basic partial-completion)))))
+
+(use-package consult
+  :bind
+  (("C-s"   . consult-line)
+   ("C-x b" . consult-buffer)
+   ("M-y"   . consult-yank-pop)
+   ("M-g g" . consult-goto-line)
+   ("M-g i" . consult-imenu)
+   ("C-c r" . consult-ripgrep)))
+
+(use-package embark
+  :bind
+  (("C-."   . embark-act)
+   ("C-;"   . embark-dwim)
+   ("C-h B" . embark-bindings)))
+
+(use-package embark-consult
+  :after (embark consult)
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
 (use-package treesit-auto
   :custom
   (treesit-auto-install 'prompt)
