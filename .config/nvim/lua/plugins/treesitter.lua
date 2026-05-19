442fa9cd419a70b8932aacf37d7f04d7eae37981
BeedPro <annoor28@hotmail.co.uk>
2026-05-19 19:12:15 +0100

    feat: update treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,6 @@
+-- This has been archived
+-- if this breaks then install:
+-- https://github.com/romus204/tree-sitter-manager.nvim
 return {
   "nvim-treesitter/nvim-treesitter",
   branch = "main",
