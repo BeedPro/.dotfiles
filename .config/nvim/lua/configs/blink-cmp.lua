5c97e0e2ba19762e3c2c7cd4359040b4898b6f32
Beed <annoor28@hotmail.co.uk>
2026-05-19 02:16:35 +0100

    feat: have C-f go down and c-b go up

--- .config/nvim/lua/configs/blink-cmp.lua
+++ .config/nvim/lua/configs/blink-cmp.lua
@@ -69,8 +69,8 @@ return {
     ["<C-n>"] = { "select_next", "fallback" },
     ["<C-p>"] = { "select_prev", "fallback" },
 
-    ["<C-f>"] = { "scroll_documentation_up", "fallback" },
-    ["<C-b>"] = { "scroll_documentation_down", "fallback" },
+    ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+    ["<C-f>"] = { "scroll_documentation_down", "fallback" },
 
     ["<C-j>"] = { "snippet_forward", "fallback" },
     ["<C-k>"] = { "snippet_backward", "fallback" },
