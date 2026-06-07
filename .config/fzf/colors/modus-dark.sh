6c09437e9e1830488352bcfa9c19de65ebe9d44b
BeedPro <annoor28@hotmail.co.uk>
2026-06-07 15:33:40 +0100

    refactor: remove teme switching and color theme directories

--- .config/fzf/colors/modus-dark.sh
+++
@@ -1,8 +0,0 @@
-export FZF_DEFAULT_OPTS=" \
---color=bg:#000000,bg+:#2f3849,spinner:#fec43f,hl:#ff5f59 \
---color=fg:#ffffff,header:#2fafff,info:#00d3d0,pointer:#feacd0 \
---color=marker:#44bc44,fg+:#ffffff,prompt:#2fafff,hl+:#ff6b55 \
---color=selected-bg:#2f3849 \
---color=border:#646464,label:#989898"
-
-tmux set-environment -g FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS" 2>/dev/null
--- .config/fzf/colors/modus-light.sh
+++
@@ -1,8 +0,0 @@
-export FZF_DEFAULT_OPTS=" \
---color=bg:#ffffff,bg+:#dae5ec,spinner:#884900,hl:#a60000 \
---color=fg:#000000,header:#0031a9,info:#005e8b,pointer:#721045 \
---color=marker:#006800,fg+:#000000,prompt:#0031a9,hl+:#a0132f \
---color=selected-bg:#dae5ec \
---color=border:#9f9f9f,label:#595959"
-
-tmux set-environment -g FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS" 2>/dev/null
