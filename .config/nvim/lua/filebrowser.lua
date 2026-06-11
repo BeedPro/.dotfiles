880a194982a8affca17729525ee2221f7c47977e
Beed <annoor28@hotmail.co.uk>
2026-06-11 18:07:23 +0100

    refactor: flatten nvim filebrowser config

--- .config/nvim/lua/filebrowser.lua
+++ .config/nvim/lua/filebrowser.lua
@@ -4,50 +4,6 @@ vim.pack.add {
 
 local M = {}
 
-function M.setup()
-  require("oil").setup {
-    delete_to_trash = true,
-    view_options = {
-      show_hidden = true,
-    },
-    columns = {},
-  }
-
-  vim.keymap.set("n", "<leader>.", function()
-    M.toggle()
-  end, { desc = "Toggle file explorer" })
-
-  vim.api.nvim_create_autocmd("VimEnter", {
-    group = vim.api.nvim_create_augroup("OilOpenOnStart", { clear = true }),
-    callback = function(data)
-      if vim.fn.isdirectory(data.file) ~= 1 then
-        return
-      end
-
-      vim.schedule(function()
-        vim.cmd.cd(data.file)
-        vim.cmd("Oil " .. vim.fn.fnameescape(data.file))
-      end)
-    end,
-  })
-
-  vim.api.nvim_create_autocmd("FileType", {
-    group = vim.api.nvim_create_augroup("OilDetailMappings", { clear = true }),
-    pattern = "oil",
-    callback = function(args)
-      vim.b[args.buf].oil_detail = vim.b[args.buf].oil_detail or false
-      vim.keymap.set("n", "gd", function()
-        vim.b[args.buf].oil_detail = not vim.b[args.buf].oil_detail
-        if vim.b[args.buf].oil_detail then
-          require("oil").set_columns { "permissions", "size", "mtime" }
-        else
-          require("oil").set_columns {}
-        end
-      end, { buffer = args.buf, desc = "Toggle file detail view" })
-    end,
-  })
-end
-
 function M.toggle()
   if vim.bo.filetype == "oil" then
     require("oil").close()
@@ -71,5 +27,47 @@ function M.set_local_file_picker(callback, augroup_name)
     end,
   })
 end
+
+require("oil").setup {
+  delete_to_trash = true,
+  view_options = {
+    show_hidden = true,
+  },
+  columns = {},
+}
+
+vim.keymap.set("n", "<leader>.", function()
+  M.toggle()
+end, { desc = "Toggle file explorer" })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = vim.api.nvim_create_augroup("OilOpenOnStart", { clear = true }),
+  callback = function(data)
+    if vim.fn.isdirectory(data.file) ~= 1 then
+      return
+    end
+
+    vim.schedule(function()
+      vim.cmd.cd(data.file)
+      vim.cmd("Oil " .. vim.fn.fnameescape(data.file))
+    end)
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("OilDetailMappings", { clear = true }),
+  pattern = "oil",
+  callback = function(args)
+    vim.b[args.buf].oil_detail = vim.b[args.buf].oil_detail or false
+    vim.keymap.set("n", "gd", function()
+      vim.b[args.buf].oil_detail = not vim.b[args.buf].oil_detail
+      if vim.b[args.buf].oil_detail then
+        require("oil").set_columns { "permissions", "size", "mtime" }
+      else
+        require("oil").set_columns {}
+      end
+    end, { buffer = args.buf, desc = "Toggle file detail view" })
+  end,
+})
 
 return M
