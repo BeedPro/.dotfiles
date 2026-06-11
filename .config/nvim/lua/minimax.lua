d185242b4a80a8cd597a60229d16335e6560b32c
BeedPro <annoor28@hotmail.co.uk>
2026-06-11 18:44:39 +0100

    feat: rename snippets to minisnippets

--- .config/nvim/lua/minimax.lua
+++ .config/nvim/lua/minimax.lua
@@ -52,7 +52,7 @@ local gen_loader = require("mini.snippets").gen_loader
 
 require("mini.snippets").setup {
   snippets = {
-    gen_loader.from_file(vim.fs.joinpath(vim.fn.stdpath "config", "snippets", "global.json")),
+    gen_loader.from_file(vim.fs.joinpath(vim.fn.stdpath "config", "minisnippets", "global.json")),
     gen_loader.from_lang(),
   },
   mappings = {
