13c8c2a47c45857fb91b8dc7e23ec2fc10534107
Beed <annoor28@hotmail.co.uk>
2026-06-11 18:15:42 +0100

    feat: set minimax

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,9 +14,9 @@ require("debugging")
 require("git")
 require("notes")
 require("filebrowser")
-require("navigation")
-require("completion")
--- require("minimax")
+-- require("navigation")
+-- require("completion")
+require("minimax")
 
 -- Options
 vim.o.spellfile = vim.fs.joinpath(vim.fn.stdpath "config", "spell", "en.utf-8.add")
