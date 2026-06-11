486075fb54a66625ae30e6f327b778fb086e6d0f
BeedPro <annoor28@hotmail.co.uk>
2026-06-11 19:42:46 +0100

    feat: move neogen out to comments.lua

--- .config/nvim/lua/editing.lua
+++ .config/nvim/lua/editing.lua
@@ -1,7 +1,6 @@
 vim.pack.add {
   "https://github.com/nvim-mini/mini.nvim",
   "https://github.com/nvim-treesitter/nvim-treesitter",
-  "https://github.com/danymat/neogen",
 }
 
 require("mini.sessions").setup {
@@ -47,55 +46,6 @@ local parsers = {
 }
 
 require("nvim-treesitter").install(parsers)
-
-require("neogen").setup {
-  snippet_engine = "luasnip",
-  languages = {
-    python = {
-      template = {
-        annotation_convention = "google_docstrings",
-        sphinx = {
-          { nil, '"""$1"""', { no_results = true, type = { "class", "func" } } },
-          { nil, '"""$1', { no_results = true, type = { "file" } } },
-          { nil, "", { no_results = true, type = { "file" } } },
-          { nil, "$1", { no_results = true, type = { "file" } } },
-          { nil, '"""', { no_results = true, type = { "file" } } },
-          { nil, "", { no_results = true, type = { "file" } } },
-          { nil, "# $1", { no_results = true, type = { "type" } } },
-          { nil, '"""$1' },
-          { nil, "" },
-          {
-            require("neogen.types.template").item.Parameter,
-            ":param %s: $1",
-            { after_each = ":type %s: $1", type = { "func" } },
-          },
-          {
-            { require("neogen.types.template").item.Parameter, require("neogen.types.template").item.Type },
-            ":param %s %s: $1",
-            { required = require("neogen.types.template").item.Tparam, type = { "func" } },
-          },
-          { require("neogen.types.template").item.ClassAttribute, ":ivar %s: $1" },
-          { require("neogen.types.template").item.Throw, ":raises %s: $1", { type = { "func" } } },
-          {
-            require("neogen.types.template").item.Return,
-            ":returns: $1",
-            { after_each = ":rtype: $1", type = { "func" } },
-          },
-          {
-            require("neogen.types.template").item.ReturnTypeHint,
-            ":returns: $1",
-            { after_each = ":rtype: %s", type = { "func" } },
-          },
-          { nil, '"""' },
-        },
-      },
-    },
-  },
-}
-
-vim.keymap.set("n", "<leader>ca", function()
-  require("neogen").generate()
-end, { desc = "Generate code annotation" })
 
 vim.api.nvim_create_autocmd("FileType", {
   group = vim.api.nvim_create_augroup("TreesitterFileTypeStart", { clear = true }),
