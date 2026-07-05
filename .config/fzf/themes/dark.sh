a0c2fc1dd68ec41b63e1774c023b174df4c0d09b
Beed Linux <beed@nimbus.communityfibre.co.uk>
2026-07-05 16:28:57 +0100

    feat: add optional --bottom

--- .config/fzf/themes/dark.sh
+++ .config/fzf/themes/dark.sh
@@ -1,8 +1,8 @@
 export FZF_DEFAULT_OPTS=" \
---color=bg:#000000,bg+:#595959,spinner:#fec43f,hl:#ff5f59 \
+--color=bg:#000000,bg+:#000000,spinner:#fec43f,hl:#ff5f59 \
 --color=fg:#ffffff,header:#2fafff,info:#00d3d0,pointer:#feacd0 \
 --color=marker:#44bc44,fg+:#ffffff,prompt:#2fafff,hl+:#ff6b55 \
---color=selected-bg:#595959 \
+--color=selected-bg:#000000 \
 --color=border:#a6a6a6,label:#595959"
 
 tmux set-environment -g FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS" 2>/dev/null
--- .config/fzf/themes/light.sh
+++ .config/fzf/themes/light.sh
@@ -1,8 +1,8 @@
 export FZF_DEFAULT_OPTS=" \
---color=bg:#ffffff,bg+:#a6a6a6,spinner:#884900,hl:#a60000 \
+--color=bg:#ffffff,bg+:#ffffff,spinner:#884900,hl:#a60000 \
 --color=fg:#000000,header:#0031a9,info:#005e8b,pointer:#721045 \
 --color=marker:#006800,fg+:#000000,prompt:#0031a9,hl+:#972500 \
---color=selected-bg:#a6a6a6 \
+--color=selected-bg:#ffffff \
 --color=border:#a6a6a6,label:#595959"
 
 tmux set-environment -g FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS" 2>/dev/null
