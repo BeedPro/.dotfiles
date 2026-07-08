809d019433fe7cb111915b88acb9a546ea0b763e
Beed Linux <beed@nimbus.communityfibre.co.uk>
2026-07-08 14:06:11 +0100

    feat: add xrandr to screenlayout

--- .screenlayout/nimbus.sh
+++ .screenlayout/nimbus.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+xrandr --output DP-0 --mode 1920x1080 --rate 143.85 --rotate left --pos 0x0 \
+       --output DP-4 --primary --mode 1920x1080 --rate 143.85 --rotate normal --pos 1080x420
+
 feh --no-fehbg \
   --bg-scale "$HOME/.config/wallpaper/current.jpg" \
   --bg-fill "$HOME/.config/wallpaper/current.jpg"
