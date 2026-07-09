592987afd122ab1149aa7e9e637864d7bbe1bc18
Beed Linux <beed@pileus.communityfibre.co.uk>
2026-07-09 09:55:27 +0100

    feat: update .screenlayout.sh for pileus

--- .screenlayout/pileus.sh
+++ .screenlayout/pileus.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-feh --no-fehbg --bg-scale $HOME/.config/wallpaper/current.jpg
+xrandr --output eDP --primary --mode 1920x1200 --pos 0x0 --rotate normal
+
+feh --no-fehbg --bg-scale "$HOME/.config/wallpaper/current.jpg"
 
 xrdb -merge <<EOF
-Xft.dpi: 120
-Xcursor.size: 32
+Xft.dpi: 96
+Xcursor.size: 24
 EOF
 
 xset s off
