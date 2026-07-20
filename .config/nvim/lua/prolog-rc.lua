e2dcdb5a189dde6b8407e1c120b4b56ae21996a7
BeedPro <beed.epistle@outlook.com>
2026-07-20 19:15:58 +0100

    feat: add back prolog support

--- .config/nvim/lua/prolog-rc.lua
+++ .config/nvim/lua/prolog-rc.lua
@@ -2,6 +2,24 @@ local lsp = require "lsp-rc"
 local quality = require "quality-rc"
 local treesitter = require "treesitter-rc"
 
+vim.api.nvim_create_autocmd("User", {
+  group = vim.api.nvim_create_augroup("PrologTreesitterParser", { clear = true }),
+  pattern = "TSUpdate",
+  callback = function()
+    require("nvim-treesitter.parsers").prolog = {
+      tier = 4,
+      install_info = {
+        url = "https://github.com/foxyseta/tree-sitter-prolog",
+        revision = "39d5e4e89f336b3bbf245f6fae18473b2e8c88a5",
+        location = "grammars/prolog",
+        queries = "grammars/prolog/queries",
+      },
+    }
+  end,
+})
+
+vim.treesitter.language.register("prolog", "prolog")
+
 lsp.enable { "prolog_ls" }
 
 treesitter.add { "prolog" }
