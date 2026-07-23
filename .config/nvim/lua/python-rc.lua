365cafdf3d055562cb967ee882633f2755e4168e
BeedPro <beed.epistle@outlook.com>
2026-07-23 15:40:51 +0100

    refactor: move python docs config

--- .config/nvim/lua/python-rc.lua
+++ .config/nvim/lua/python-rc.lua
@@ -1,6 +1,7 @@
 local lsp = require "lsp-rc"
 local mason = require "mason-rc"
 local quality = require "quality-rc"
+local documentation = require "documentation-rc"
 local treesitter = require "treesitter-rc"
 require "debugging-rc"
 
@@ -23,6 +24,50 @@ quality.formatters {
 quality.linters {
   python = { "ruff" },
   htmldjango = { "djlint" },
+}
+
+local template = require("neogen.types.template").item
+
+documentation.languages {
+  python = {
+    template = {
+      annotation_convention = "google_docstrings",
+      sphinx = {
+        { nil, '"""$1"""', { no_results = true, type = { "class", "func" } } },
+        { nil, '"""$1', { no_results = true, type = { "file" } } },
+        { nil, "", { no_results = true, type = { "file" } } },
+        { nil, "$1", { no_results = true, type = { "file" } } },
+        { nil, '"""', { no_results = true, type = { "file" } } },
+        { nil, "", { no_results = true, type = { "file" } } },
+        { nil, "# $1", { no_results = true, type = { "type" } } },
+        { nil, '"""$1' },
+        { nil, "" },
+        {
+          template.Parameter,
+          ":param %s: $1",
+          { after_each = ":type %s: $1", type = { "func" } },
+        },
+        {
+          { template.Parameter, template.Type },
+          ":param %s %s: $1",
+          { required = template.Tparam, type = { "func" } },
+        },
+        { template.ClassAttribute, ":ivar %s: $1" },
+        { template.Throw, ":raises %s: $1", { type = { "func" } } },
+        {
+          template.Return,
+          ":returns: $1",
+          { after_each = ":rtype: $1", type = { "func" } },
+        },
+        {
+          template.ReturnTypeHint,
+          ":returns: $1",
+          { after_each = ":rtype: %s", type = { "func" } },
+        },
+        { nil, '"""' },
+      },
+    },
+  },
 }
 
 vim.pack.add {
