8ecfaba1aaf60b92c64c70dfd4e3ed282d97af59
BeedPro <beed.epistle@outlook.com>
2026-07-25 00:07:20 +0100

    feat: remove padding

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -2,3 +2,7 @@
 
 ;; Work around distro site-start files that expect this variable.
 (defvar flavor 'emacs)
+
+;; Let the window manager size frames to exact pixels, avoiding unused strips
+;; when the frame width is not an exact multiple of the font cell width.
+(setq frame-resize-pixelwise t)
